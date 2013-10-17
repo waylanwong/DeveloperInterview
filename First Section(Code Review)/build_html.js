705b842fb524e8368959331fdcbe6f3a891dfecf
Waylan Wong <wong.waylan@gmail.com>
2013-10-16 20:32:14 -0700

    modified build_html.js file to include code that would test if the applicant knows about javascript 'hoisting', if they know of different ways to insert DOM elements

--- First Section(Code Review)/build_html.js
+++ First Section(Code Review)/build_html.js
@@ -1,25 +1,3 @@
-//Option 1
-function build_html_1(dom_obj){
-	var html_array = [];
-	htmlarray.push('<li data-id="1" class="selected">');
-	htmlarray.push('<p class="contract-date">December 10 2013</p>');
-	htmlarray.push('<div class="contract-status">Confirmed</div>');
-	htmlarray.push('</li>');
-	var html_string = html_array.join('');
-	dom_obj.append(html_string);
-}
-
-//Option 2
-function build_html_2(dom_obj){
-	var html_string = '';
-	html_string += '<li data-id="1" class="selected">';
-	html_string += '<p class="contract-date">December 10 2013</p>';
-	html_string += '<div class="contract-status">Confirmed</div>';
-	html_string += '</li>';
-	dom_obj.append(html_string);
-}
-
-
 $(document).ready(function(){
 	
 	$('#btn_january').click(function(){
@@ -40,7 +18,26 @@ $(document).ready(function(){
 		dom_contractlist.append(html_string);
 	});
 	
-	$(document).on('contract_month_selected', function() {
+	$(document).on('contract_month_selected', function() {		
 		
+		calculateMonth();	
+		
+		var calculateMonth = function() {
+			
+			if ($(this).data('month') === 'January') {
+				return false;
+			} else if ($(this).data('month') == 'February') {
+				return true;
+			}
+			
+			var html_string = '';
+			html_string += '<li data-id="1" class="selected">';
+			html_string += '<p class="contract-date">'+ $(this).data('month') +'</p>';
+			html_string += '<div class="contract-status">Started</div>';
+			html_string += '</li>';	
+			dom_contractlist.append(html_string);
+		};
 	});
+	
+	
 });
